SEO7077/strawberry-django-plus

commit c39900803921ca6efe6e6479e44b242076ff8889

Replace error message formatting characters with parameters

strawberry_django_plus/mutations/fields.py
@@ -72,7 +72,7 @@ def _get_validation_errors(error: Exception):
         for e in error.error_list:
             yield OperationMessage(
                 kind=kind,
-                message=e.message,
+                message=e.message % e.params,
             )
     else:
         msg = getattr(error, "msg", None)

commit ccf5bd511c04cd33dd54a43d23596c8616df6bc2

fix: only format message with params if params is not empty
This fixes a regression in c39900803921ca6efe6e6479e44b242076ff8889

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "strawberry-django-plus"
-version = "2.3.1"
+version = "2.3.2"
 description = "Enhanced Strawberry GraphQL integration with Django"
 authors = ["Thiago Bellini Ribeiro <thiago@bellini.dev>"]
 license = "MIT"

strawberry_django_plus/mutations/fields.py
@@ -72,7 +72,7 @@ def _get_validation_errors(error: Exception):
         for e in error.error_list:
             yield OperationMessage(
                 kind=kind,
-                message=e.message % e.params,
+                message=e.message % e.params if e.params else e.message,
             )
     else:
         msg = getattr(error, "msg", None)